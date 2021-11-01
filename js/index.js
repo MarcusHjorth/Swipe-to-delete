d746443830bb0a4b74e7d41dec8cebb117e229af
MarcusHjorth <U773559@rts-365.dk>
2021-11-01 15:05:35 +0100

    Næsten godt nok

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@ let jokeItem = document.querySelector('.jokeItem');
 
 let touchCordinateStart;
 let touchCordinateMove;
+let touchCordinateEnd;
+
+let deleteButtonWidth = (window.screen.width * 0.4);
 
 jokeItem.addEventListener('touchstart', (event) => {
     touchCordinateStart = event.touches[0].clientX;
@@ -9,16 +12,23 @@ jokeItem.addEventListener('touchstart', (event) => {
 
 jokeItem.addEventListener('touchmove', (event) => {
     touchCordinateMove = Math.floor(event.touches[0].clientX);
-    if (touchCordinateMove < touchCordinateStart) {
+    if (touchCordinateMove < touchCordinateStart && 
+        touchCordinateMove > touchCordinateStart - deleteButtonWidth) {
         
         jokeItem.style.transform = `translateX(${
-            touchCordinateMove - touchCordinateStart
-        }px)`;
-        
-        console.log(touchCordinateMove);
+            touchCordinateMove - touchCordinateStart}px)`;
+
+        //console.log(touchCordinateMove);
     }
-
-    
-
-
 });
+
+
+jokeItem.addEventListener('touchend', (event) => {
+    touchCordinateEnd = Math.floor(event.changedTouches[0].clientX);
+    if(touchCordinateEnd < touchCordinateStart - deleteButtonWidth / 2) {
+        jokeItem.style.transform = `translateX(-${deleteButtonWidth}px)`
+    } else {
+        jokeItem.style.transform = `translateX(${event.target.offsetLeft})`
+    }
+    
+})
